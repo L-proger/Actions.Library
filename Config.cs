1fdae8e0041c10765cdaf0337d17fa9ec9c58759
L-proger <l-proger@yandex.ru>
2024-05-26 16:25:38 +0300

    Add nuspec file serialization

--- Config.cs
+++ Config.cs
@@ -1,11 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Actions.Library {
     public static class Config {
-       
+        private static string PropertyNamePrefix = "Actions_";
+
+        private static string? Get([CallerMemberName]string? name = null) {
+            if(string.IsNullOrWhiteSpace(name)) {
+                throw new ArgumentNullException(nameof(name));
+            }
+            var result = System.Environment.GetEnvironmentVariable(PropertyNamePrefix + name);
+            return result;
+        }
+        private static void Set(string? value, [CallerMemberName] string? name = null) {
+            if (string.IsNullOrWhiteSpace(name)) {
+                throw new ArgumentNullException(nameof(name));
+            }
+            System.Environment.SetEnvironmentVariable(PropertyNamePrefix + name, value);
+        }
+
+
+        public static string? Nuget_RepoAuth { get { return Get(); } set { Set(value); } }
+        public static string? Nuget_RepoName { get { return Get(); } set { Set(value); } }
     }
 }
---
+++ Nuget/NuspecFile.cs
@@ -0,0 +1,57 @@
+﻿using Actions.Nuget.Nuspec;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Actions.Nuget {
+
+
+
+
+    public class NuspecFile {
+        public Nuspec.Package package;
+
+        public NuspecFile() {
+            package = new Nuspec.Package();
+        }
+        public NuspecFile(string id, string version, string authors, string owners, Uri projectUrl, bool requireLicenseAcceptance, string description, string[] tags, string license) {
+            package = new Nuspec.Package();
+            package.Metadata = new Nuspec.PackageMetadata();
+            package.Metadata.Authors = authors;
+            package.Metadata.Owners = owners;
+            package.Metadata.Id = id;
+            package.Metadata.Version = version;
+            package.Metadata.ProjectUrl = projectUrl.ToString();
+            package.Metadata.RequireLicenseAcceptance = requireLicenseAcceptance;
+            package.Metadata.Description = description;
+            package.Metadata.Tags = string.Join(" ", tags);
+            package.Metadata.License = new PackageMetadataLicense();
+            package.Metadata.License.Value = license; 
+        }
+
+        public void Serialize(Stream outStream) {
+            XmlSerializer serializer = new XmlSerializer(typeof(Nuspec.Package), "http://schemas.microsoft.com/packaging/2010/07/nuspec.xsd");
+            serializer.Serialize(outStream, package);
+        }
+
+        public void SerializeToFile(string path) {
+            var outFileStream = System.IO.File.Open(path, FileMode.Create, FileAccess.Write);
+            Serialize(outFileStream);
+            outFileStream.Close();
+        }
+
+        public void Deserialize(Stream inStream) {
+            XmlSerializer serializer = new XmlSerializer(typeof(Nuspec.Package), "http://schemas.microsoft.com/packaging/2010/07/nuspec.xsd");
+            package = (Nuspec.Package)serializer.Deserialize(inStream);
+        }
+
+        public void DeserializeFromFile(string path) {
+            var inFileStream = System.IO.File.Open(path, FileMode.Open, FileAccess.Read);
+            Deserialize(inFileStream);
+            inFileStream.Close();
+        }
+    }
+}
